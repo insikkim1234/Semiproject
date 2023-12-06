77eeb1a2425473af16b49f7f14fc3ba7a64c125e
DESKTOP-DE6PPJ4\82103 <ckdlgnfus@naver.com>
2023-12-06 16:36:55 +0900

    게시판 기본 틀

---
+++ src/main/java/semi/board/BoardController.java
@@ -0,0 +1,14 @@
+package semi.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+	 @GetMapping("/board")
+	 public String boardList() {
+		 return "board/boardlist";
+	 }
+	 
+}
