ac945af8f60725291e834fdbb386495564de687a
dhdnjscks <dhdnjscks@gmail.com>
2023-12-08 16:25:18 +0900

    tile 버전 변경 마이너 변경

--- src/main/java/bitcamp/project/semi/HomeController.java
+++ src/main/java/bitcamp/project/semi/HomeController.java
@@ -8,8 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Handles requests for the application home page.
@@ -22,7 +21,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@GetMapping(value = "/")
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
