23cfb47f316c9aa1149e2d64d53c15d266333027
cyanO94 <dhdnjscks@gmail.com>
2023-12-11 18:43:12 +0900

    레시피 순서 api 추가

--- src/main/java/semi/controller/RecipeRestController.java
+++ src/main/java/semi/controller/RecipeRestController.java
@@ -4,33 +4,34 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import semi.dao.RecipeDao;
+import semi.dao.RecipeOrderDao;
 import semi.dto.RecipeDto;
+import semi.dto.RecipeOrderDto;
+import semi.service.RecipeOrderService;
 
 @RestController
 public class RecipeRestController {
 	@Autowired
 	private RecipeDao recipeDao;
-	
+
+	@Autowired
+	private RecipeOrderService recipeOrderService;
+
 	//dto 불러오기
 	@GetMapping("/recipe/view")
 	public List<RecipeDto> getAllRecipe(){
 		return recipeDao.getAlltRecipe();
 	}
 
-	@GetMapping("/recipe/selectAllDataTest")
-	public String selectAllDataTest(){
-		List<RecipeDto> recipeData = recipeDao.getAlltRecipe();
 
-		StringBuilder sb = new StringBuilder();
-		for (RecipeDto data: recipeData) {
-			sb.append("<p>");
-			sb.append(data);
-			sb.append("</p>");
-		}
+	// region RecipeOrder
 
-		return sb.toString();
+	@GetMapping("recipe/orderView/{recipeIdx}")
+	public List<RecipeOrderDto> getRecipeOrderAll(@PathVariable int recipeIdx) {
+		return recipeOrderService.getRecipeOrderById(recipeIdx);
 	}
 }
