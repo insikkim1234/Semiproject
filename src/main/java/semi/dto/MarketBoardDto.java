c104072ceb1370b70a4c2194ca293bb6f10a93f4
KIMINSIK <insikkim2439@gmail.com>
2023-12-13 12:43:09 +0900

    MarketBoard에 들어가는지 보자

--- src/main/java/semi/dto/MarketBoardDto.java
+++ src/main/java/semi/dto/MarketBoardDto.java
@@ -2,17 +2,27 @@ package semi.dto;
 
 import java.sql.Timestamp;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class MarketBoardDto {
+	@JsonProperty("nBoardSeq")
 	private int nBoardSeq;
+	@JsonProperty("nUserSeq")
 	private int nUserSeq;
+	@JsonProperty("sBoardTitle")
 	private String sBoardTitle;
+	@JsonProperty("sBoardImage")
 	private String sBoardImage;
+	@JsonProperty("nBoardReplyCount")
 	private int nBoardReplyCount;
+	@JsonProperty("nBoardViewCount")
 	private int nBoardViewCount;
+	@JsonProperty("dtCreateDate")
 	private Timestamp dtCreateDate;	
+	@JsonProperty("dtUpdateDate")
 	private Timestamp dtUpdateDate;	
 }
 
